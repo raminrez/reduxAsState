c5fd7fde5016f70e7ca3539eb8ff9d2116ff680b
raminrez <ramin12207@gmail.com>
2019-07-04 03:06:29 +0430

    normalize data structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { schema, normalize } from "normalizr";
 import { createLogger } from "redux-logger";
 import { Provider, connect } from "react-redux";
 
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 // logger
@@ -15,19 +16,42 @@ const logger = createLogger();
 const TODO_ADD = "TODO_ADD";
 const TODO_TOGGLE = "TODO_TOGGLE";
 const FILTER_SET = "FILTER_SET";
+const ASSIGNED_TO_CHANGE = "ASSIGNED_TO_CHANGE";
+//Initialize states
+const todos = [
+  { id: "1", name: "Hands On: Redux Standalone with advanced Actions" },
+  { id: "2", name: "Hands On: Redux Standalone with advanced Reducers" },
+  { id: "3", name: "Hands On: Bootstrap App with Redux" },
+  { id: "4", name: "Hands On: Naive Todo with React and Redux" },
+  { id: "5", name: "Hands On: Sophisticated Todo with React and Redux" },
+  { id: "6", name: "Hands On: Connecting State Everywhere" },
+  { id: "7", name: "Hands On: Todo with advanced Redux" },
+  { id: "8", name: "Hands On: Todo but more Features" },
+  { id: "9", name: "Hands On: Todo with Notifications" },
+  { id: "10", name: "Hands On: Hacker News with Redux" }
+];
+
+//schemas
+const todoSchema = new schema.Entity("todo");
+
+//normalized schemas
+const normalizedTodos = normalize(todos, [todoSchema]);
+const initialTodoState = {
+  entities: normalizedTodos.entities.todo,
+  ids: normalizedTodos.result
+};
 
 //reducers
-const todos = [
-  { id: "0", name: "learn redux" },
-  { id: "1", name: "learn mobx" }
-];
-function todoReducer(state = todos, action) {
+function todoReducer(state = initialTodoState, action) {
   switch (action.type) {
     case TODO_ADD: {
       return applyAddTodo(state, action);
     }
     case TODO_TOGGLE: {
       return applyToggleTodo(state, action);
+    }
+    case ASSIGNED_TO_CHANGE: {
+      return applyChangeAssignedTo(state, action);
     }
     default:
       return state;
@@ -36,13 +60,30 @@ function todoReducer(state = todos, action) {
 
 function applyAddTodo(state, action) {
   const todo = { ...action.todo, completed: false };
-  return [...state, todo];
+  const entities = { ...state.entities, [todo.id]: todo };
+  const ids = [...state.ids, action.todo.id];
+  return { ...state, entities, ids };
 }
+
 function applyToggleTodo(state, action) {
-  return state.map(todo =>
-    todo.id === action.todo.id ? { ...todo, completed: !todo.completed } : todo
-  );
+  const id = action.todo.id;
+  const todo = state.entities[id];
+  const toggledTodo = { ...todo, completed: !todo.completed };
+  const entities = { ...state.entities, [id]: toggledTodo };
+  return { ...state, entities };
 }
+
+function applyChangeAssignedTo(state, action) {
+  return state.map(todo => {
+    if (todo.id === action.payload.todoId) {
+      const assignedTo = { ...todo.assignedTo, name: action.payload.name };
+      return { ...todo, assignedTo };
+    } else {
+      return todo;
+    }
+  });
+}
+
 function filterReducer(state = "SHOW_ALL", action) {
   switch (action.type) {
     case FILTER_SET: {
@@ -93,11 +134,11 @@ function TodoApp() {
   );
 }
 
-function TodoList({ todos }) {
+function TodoList({ todosAsIds }) {
   return (
     <div>
-      {todos.map(todo => (
-        <ConnectedTodoItem key={todo.id} todo={todo} />
+      {todosAsIds.map(todoId => (
+        <ConnectedTodoItem key={todoId} todoId={todoId} />
       ))}
     </div>
   );
@@ -115,23 +156,37 @@ function TodoItem({ todo, onToggleTodo }) {
   );
 }
 
-function mapStateToProps(state) {
+function mapStateToPropsList(state) {
   return {
-    todos: state.todoState
+    todosAsIds: state.todoState.ids
   };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapStateToPropsItem(state, props) {
+  return {
+    todo: state.todoState.entities[props.todoId]
+  };
+}
+
+function mapDispatchToPropsItem(dispatch) {
   return {
     onToggleTodo: id => dispatch(doToggleTodo(id))
   };
 }
 
-const ConnectedTodoList = connect(mapStateToProps)(TodoList);
+const ConnectedTodoList = connect(mapStateToPropsList)(TodoList);
 const ConnectedTodoItem = connect(
-  null,
-  mapDispatchToProps
+  mapStateToPropsItem,
+  mapDispatchToPropsItem
 )(TodoItem);
+
+// store.dispatch({
+//   type: ASSIGNED_TO_CHANGE,
+//   payload: {
+//     todoId: "0",
+//     name: "Dan Abramov and Andrew Clark"
+//   }
+// });
 
 ReactDOM.render(
   <Provider store={store}>
